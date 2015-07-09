8b0e852eb46e8d518895660f64029b3092ea48a7
Victor Pereira Luna <victorluna22@gmail.com>
2015-07-09 16:06:53 -0300

    clear repository

--- www/js/services.js
+++
@@ -1,8 +0,0 @@
-var hospitalsAddress = angular.module('hospitalsAddress', ['ngResource']);
-
-hospitalsAddress.factory('Hospital', ['$resource',
-  function($resource){
-      return $resource('/jsons/hospital.json', {}, {
-        query: {}
-      });
-  }]);
